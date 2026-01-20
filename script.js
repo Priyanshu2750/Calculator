adfc0daefeeb957c41fb4b03a66f76c97749e584
Priyanshu Barnwal <pbarnwal275@gmail.com>
2026-01-20 20:42:52 +0530

    Implement operate function logic
    Validate inputs, execute operation and output result for testing

--- script.js
+++ script.js
@@ -13,3 +13,42 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate() {
+  let num1 = +prompt("Enter a number", "");
+  if (isNaN(num1) === true) {
+    alert("Enter a number!");
+    return;
+  }
+
+  let operator = prompt("Enter an operator", "");
+  if (
+    operator !== "+" &&
+    operator !== "-" &&
+    operator !== "*" &&
+    operator !== "/"
+  ) {
+    alert("Enter a valid operator!");
+    return;
+  }
+
+  let num2 = +prompt("Enter another number", "");
+  if (isNaN(num2) === true) {
+    alert("Enter a number!");
+    return;
+  }
+
+  let result;
+
+  if (operator === "+") {
+    result = add(num1, num2);
+  } else if (operator === "-") {
+    result = subtract(num1, num2);
+  } else if (operator === "*") {
+    result = multiply(num1, num2);
+  } else if (operator === "/") {
+    result = divide(num1, num2);
+  }
+
+  console.log(result);
+}
